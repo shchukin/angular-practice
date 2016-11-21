8540c4fdb4f681e468bc1f160178cc322626a595
Anton Shchukin <a.a.shchukin@gmail.com>
2016-11-21 21:15:31 +0700

    try more complex example of data binding

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,11 @@
-var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
+var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+
+    $scope.handle = '';
+
+    $scope.lowerhandle = function () {
+        return $filter('lowercase')( $scope.handle );
+    }
 
 }]);
