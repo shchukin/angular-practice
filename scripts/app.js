91a3feabd985d0cdb40d1aa15f11bed2d61db811
Anton Shchukin <a.a.shchukin@gmail.com>
2016-11-21 20:46:26 +0700

    init bootstrap dummy project

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,4 @@ var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
 myApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
 
-    $scope.name = 'Tony';
-
-    $timeout(function () {
-        $scope.name = 'Everybody';
-    }, 3000);
-
 }]);
