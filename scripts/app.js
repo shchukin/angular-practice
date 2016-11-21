0e810d31dc07e0f3c0b8b89e2e6677e4c7aa3414
Anton Shchukin <a.a.shchukin@gmail.com>
2016-11-21 12:56:38 +0700

    try external modules and services

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function ($scope, $log, $filter) {
+myApp.controller('mainController', function ($scope, $log, $filter, $resource) {
 
     $scope.name = 'Tony';
     $scope.formattedName = $filter('uppercase')($scope.name);
 
     $log.info( $scope.name );
     $log.info( $scope.formattedName );
+
+    console.log( $resource );
 
 });
